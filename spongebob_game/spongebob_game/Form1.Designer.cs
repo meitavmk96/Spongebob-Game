cf9040bdaad3f55ac5c7d503089eaba5442fa772
meitavmk96 <meitavmk96@gmail.com>
2022-12-04 20:55:58 +0200

    base

---
+++ spongebob_game/spongebob_game/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace spongebob_game
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbl_health = new System.Windows.Forms.Label();
+            this.pb_health = new System.Windows.Forms.ProgressBar();
+            this.spongebob = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.lbl_points = new System.Windows.Forms.Label();
+            this.cbo = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.spongebob)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbl_health
+            // 
+            this.lbl_health.AutoSize = true;
+            this.lbl_health.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_health.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lbl_health.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_health.Location = new System.Drawing.Point(430, 26);
+            this.lbl_health.Name = "lbl_health";
+            this.lbl_health.Size = new System.Drawing.Size(81, 25);
+            this.lbl_health.TabIndex = 2;
+            this.lbl_health.Text = "Health:";
+            // 
+            // pb_health
+            // 
+            this.pb_health.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pb_health.Location = new System.Drawing.Point(517, 31);
+            this.pb_health.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pb_health.Name = "pb_health";
+            this.pb_health.Size = new System.Drawing.Size(196, 20);
+            this.pb_health.TabIndex = 3;
+            this.pb_health.Value = 100;
+            this.pb_health.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pb_health_MouseClick);
+            this.pb_health.MouseHover += new System.EventHandler(this.pb_health_MouseHover);
+            // 
+            // spongebob
+            // 
+            this.spongebob.BackColor = System.Drawing.Color.Transparent;
+            this.spongebob.Image = global::spongebob_game.Properties.Resources.spongebob;
+            this.spongebob.Location = new System.Drawing.Point(1, 336);
+            this.spongebob.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.spongebob.Name = "spongebob";
+            this.spongebob.Size = new System.Drawing.Size(105, 134);
+            this.spongebob.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.spongebob.TabIndex = 5;
+            this.spongebob.TabStop = false;
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 20;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // lbl_points
+            // 
+            this.lbl_points.AutoSize = true;
+            this.lbl_points.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_points.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lbl_points.Location = new System.Drawing.Point(29, 26);
+            this.lbl_points.Name = "lbl_points";
+            this.lbl_points.Size = new System.Drawing.Size(77, 25);
+            this.lbl_points.TabIndex = 10;
+            this.lbl_points.Text = "points:";
+            // 
+            // cbo
+            // 
+            this.cbo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.cbo.FormattingEnabled = true;
+            this.cbo.Items.AddRange(new object[] {
+            "flowers background",
+            "krusty krab background",
+            "nigboorhood background"});
+            this.cbo.Location = new System.Drawing.Point(867, 23);
+            this.cbo.Name = "cbo";
+            this.cbo.Size = new System.Drawing.Size(296, 28);
+            this.cbo.TabIndex = 11;
+            this.cbo.SelectedIndexChanged += new System.EventHandler(this.cbo_SelectedIndexChanged);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::spongebob_game.Properties.Resources.background;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1195, 471);
+            this.Controls.Add(this.cbo);
+            this.Controls.Add(this.lbl_points);
+            this.Controls.Add(this.spongebob);
+            this.Controls.Add(this.pb_health);
+            this.Controls.Add(this.lbl_health);
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.Name = "Form";
+            this.Text = "spongebob game";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.spongebob)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_health;
+        private System.Windows.Forms.ProgressBar pb_health;
+        private System.Windows.Forms.PictureBox spongebob;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label lbl_points;
+        private System.Windows.Forms.ComboBox cbo;
+    }
+}
+
---
+++ spongebob_game/spongebob_game/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace spongebob_game
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        int points = 0;
+        bool GoLeft, GoRight;
+        int Speed = 20;
+        const int SpongebobTop = 210;
+        const int SpongebobMaxTop = SpongebobTop - 180;
+        const int ObjectSpeed = 10;
+        Random randNum;
+
+        List<PictureBox> Jellys = new List<PictureBox>();
+        List<PictureBox> planks = new List<PictureBox>();
+
+        public Form()
+        {
+            InitializeComponent();
+            randNum = new Random(this.Width - spongebob.Left); // יצירת מדוזות ופלקטונים במיקום רנדומלי
+            for (int i = 0; i < 3; i++)
+            {
+                CreateJellyfish();
+                Createplankton();
+            }
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.L)
+            {
+                GoLeft = true;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.R)
+            {
+                GoRight = true;
+            }
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left || e.KeyCode == Keys.L)
+            {
+                GoLeft = false;
+            }
+            if (e.KeyCode == Keys.Right || e.KeyCode == Keys.R)
+            {
+                GoRight = false;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (pb_health.Value <= 0) // סיום משחק שהבריאות שווה לאפס 
+            {
+                timer.Stop();
+                GameOver();
+                return;
+            }
+            if (planks.Count < 3)
+            {
+                int newPlanks = randNum.Next(0, 3 - planks.Count + 1);
+                if (newPlanks > 0)
+                    Createplankton();
+            }
+            if (Jellys.Count < 3)
+            {
+                int newJellys = randNum.Next(0, 5 - Jellys.Count + 1);
+                if (newJellys > 0)
+                    CreateJellyfish();
+            }
+            if (GoLeft == true && spongebob.Left > 0) // הגדרת תנועה ימינה שמאלה לבובספוג
+            {
+                spongebob.Left -= Speed;
+            }
+
+
+            if (GoRight == true && spongebob.Left + spongebob.Width < this.ClientSize.Width)
+            {
+                spongebob.Left += Speed;
+            }
+
+
+            lbl_points.Text = "points: " + points; // עדכון נקודות
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox jelly && (string)jelly.Tag == "jellys") // אם בובספוג נתקל במדוזה
+                {
+                    bool hit = spongebob.Bounds.IntersectsWith(x.Bounds);
+                    if (hit || jelly.Top >= this.Height)
+                    {
+                        this.Controls.Remove(jelly);
+                        Jellys.Remove(jelly);
+                        jelly.Dispose();
+                        if (hit && pb_health.Value != 0)
+                            pb_health.Value -= 20;
+                    }
+                    else
+                        x.Top = Math.Min(this.Height, x.Top + randNum.Next(5, ObjectSpeed));
+                }
+
+                if (x is PictureBox plankton && (string)plankton.Tag == "plankton") // אם בובספוג נתקל בפלנקטון
+                {
+                    bool hit = spongebob.Bounds.IntersectsWith(plankton.Bounds);
+                    if (hit || plankton.Top >= this.Height)
+                    {
+                        this.Controls.Remove(plankton);
+                        planks.Remove(plankton);
+                        plankton.Dispose();
+                        if (hit)
+                            points += 10;
+                    }
+                    else
+                        x.Top = Math.Min(this.Height, x.Top + randNum.Next(5, ObjectSpeed));
+                }
+            }
+        }
+
+        private void CreateJellyfish() // פונקציה ליצירת אובייקט מדוזה 
+        {
+            PictureBox jelly = new PictureBox();
+            jelly.BackColor = System.Drawing.Color.Transparent;
+            jelly.Image = global::spongebob_game.Properties.Resources.jellyfish;
+            jelly.Top = 0;
+            jelly.Left = randNum.Next(0, this.Width);
+            jelly.Name = "jelly1";
+            jelly.Size = new System.Drawing.Size(50, 58);
+            jelly.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            jelly.TabIndex = 11;
+            jelly.TabStop = false;
+            jelly.Tag = "jellys";
+
+            Jellys.Add(jelly);
+            this.Controls.Add(jelly);
+            jelly.BringToFront();
+        }
+
+        private void Createplankton() // פונקציה ליצירת אובייקט פלקטון 
+        {
+            PictureBox plank = new PictureBox();
+            plank.BackColor = System.Drawing.Color.Transparent;
+            plank.Image = global::spongebob_game.Properties.Resources.plankton;
+            plank.Top = 0;
+            plank.Left = randNum.Next(0, this.Width);
+            plank.Name = "plankton";
+            plank.Size = new System.Drawing.Size(50, 58);
+            plank.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            plank.TabIndex = 14;
+            plank.TabStop = false;
+            plank.Tag = "plankton";
+
+            planks.Add(plank);
+            this.Controls.Add(plank);
+        }
+
+        private void RestartGame()//פונקצית אתחול משחק
+        {
+            //מחיקת אובייקטים מהמשחק הקודם 
+            foreach (Control X in this.Controls)
+            {
+                if (X is PictureBox && ((string)X.Tag == "jellys" || (string)X.Tag == "plankton"))
+                {
+                    this.Controls.Remove(X);
+                    ((PictureBox)X).Dispose();
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                CreateJellyfish();
+                Createplankton();
+            }
+
+            spongebob.Location = new System.Drawing.Point(1, SpongebobTop); // מיקום בובספוג מחדש
+
+            // אתחול משתנים
+            points = 0;
+            pb_health.Value = 100;
+            GoLeft = false;
+            GoRight = false;
+
+            timer.Start();
+        } 
+
+        private void pb_health_MouseHover(object sender, EventArgs e) // שינוי תצוגת עכבר על הפקד
+        {
+            pb_health.Cursor = Cursors.Hand;
+        }
+
+        private void pb_health_MouseClick(object sender, MouseEventArgs e) // שינוי תצוגה של הפקד בלחיצת עכבר 
+        {
+            pb_health.Style = ProgressBarStyle.Continuous;
+        }
+
+        private void cbo_SelectedIndexChanged(object sender, EventArgs e) // אופציות לשינוי רקע המשחק בקומבו בוקס
+        {
+            string txt;
+            txt = cbo.Text;
+            if(txt == "flowers background")
+                Form.ActiveForm.BackgroundImage = global::spongebob_game.Properties.Resources.flowers_background;
+            if (txt == "krusty krab background")
+                Form.ActiveForm.BackgroundImage = global::spongebob_game.Properties.Resources.krusty_krab_background;
+            if (txt == "nigboorhood background")
+                Form.ActiveForm.BackgroundImage = global::spongebob_game.Properties.Resources.nigboorhood_background;
+        }
+
+        private void GameOver()
+        {
+            MessageBox.Show("Game over! \nbetter luck next time...");
+            RestartGame();
+        } // הודעה למשתמש שנגמר המשחק ושימוש בפונקצית האתחול
+    }
+}
